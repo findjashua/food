2b8419517297aa9a1dd1f659094ab47c32fcb26d
Jashua Gupta <findjashua@gmail.com>
2013-12-19 08:28:16 -0800

    init

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+import requests
+import json
+import pymongo
+conn = pymongo.Connection()
+db = conn.food
+
+@app.route('/home')
+def hello_world():
+	return render_template('home.html')
+
+@app.route('/init')
+def init():
+	r = requests.get('http://data.sfgov.org/resource/rqzj-sfat.json')
+	trucks = r.json()
+	for truck in trucks:
+		if 'latitude' in truck and 'longitude' in truck:
+			truck['loc'] = [float(truck['latitude']), float(truck['longitude'])]
+	db.trucks.insert(trucks)
+	return json.dumps(trucks)
+
+@app.route('/near/<lat>/<lng>')
+def nearby(lat, lng):
+	[lat, lng] = [float(loc) for loc in [lat, lng]]
+	trucks = db.trucks.find({'loc': {'$near': [lat, lng]}}).limit(100)
+	trucks = [delete_loc(truck) for truck in trucks]
+	return json.dumps(trucks)
+
+@app.route('/within/<swlat>/<swlng>/<nelat>/<nelng>')
+def within(swlat, swlng, nelat, nelng):
+	[swlat, swlng, nelat, nelng] = [float(loc) for loc in [swlat, swlng, nelat, nelng]]
+	trucks = db.trucks.find({'loc': {'$within': {'$box': [[swlat, swlng], [nelat, nelng]]}}})
+	trucks = [delete_loc(truck) for truck in trucks]
+	return json.dumps(trucks)
+
+def delete_loc(truck):
+	del truck['_id']
+	if truck.has_key('loc'):
+		del truck['loc']
+	return truck
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
